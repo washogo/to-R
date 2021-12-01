bd71fbd0358b0b7b13f3d963772b038a461f27bd
washogo <aiz4338150@gmail.com>
2021-12-01 15:44:12 +0900

    [Start]to-RでのReact学習

---
+++ src/components/Counter.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <p>Counter:{count}</p>
+      <input
+        type="button"
+        value="ボタンです"
+        // onClick={() => setCount(count + 1)}
+        onClick={() => setCount(prevCount => prevCount + 1)}
+        /*上記２つは同じ挙動*/
+      />
+    </>
+  );
+};
+
+export default Counter;
