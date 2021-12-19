cd455cd1ef812da13ae8eb75c96fab06c4a7f343
washogo <aiz4338150@gmail.com>
2021-12-19 23:52:33 +0900

    created App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
-import { useState } from 'react-redux';
+import { useState } from 'react';
 import "./styles.css";
 
 const App = () => {
   const [todos, setTodos] = useState([]);
   const [todo, setTodo] = useState("");
-}
+
+  const handleInputChange = (e) => {
+    setTodo(e.target.value);
+  }
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (todo !== ""){
+      setTodos([...todos,{
+        id: todos.length + 1,
+        text: todo.trim()
+      }
+      ]);
+    }
+    
+    setTodo("");
+  }
+
+  
+  return(
+    <>
+      <div className="App">
+        <form onSubmit={handleFormSubmit}>
+          <input 
+            name="todo"
+            type="text"
+            placeholder="Create a new todo"
+            value={todo}
+            onChange={handleInputChange}
+          />
+        </form>
+        <ul className="todo-list">
+          {todos.map(todo => (<li key={todo.id}>{todo.text}</li>))}
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default App;
