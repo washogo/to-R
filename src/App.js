aadac8968763191cebf302a857be505a3dbf3e99
washogo <aiz4338150@gmail.com>
2021-12-19 20:58:10 +0900

    [Start]App.js_模写はじめ

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
-import React, {useReducer} from 'react';
-import Header from './Header';
-import Body from './Body';
-import Form from './Form';
-
-const initialState = {
-  name: 'to-R Media'
-}
-
-const reducer = (state, action) => {
-  switch(action.type){
-    case 'CHANGE_NAME':
-      return{
-        ...state, name: action.payload
-      };
-    default:
-      return state
-  }
-}
-
-export const SiteContext = React.createContext();
-
-const SiteProvider = ({children}) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return <SiteContext.Provider value={{state,dispatch}}>
-    {children}
-  </SiteContext.Provider>
-}
+import { useState } from 'react-redux';
+import "./styles.css";
 
 const App = () => {
-  return(
-    <SiteProvider>
-      <Header/>
-      <Body/>
-      <Form/>
-    </SiteProvider>
-  )
-};
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+}
 
 export default App;
